95c226cdd62136fae2733ece3be9396e7faed763
Eduard-Andrei Popa <eduard2popa@gmail.com>
2020-03-27 13:57:50 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,18 +1,6 @@
-import math
-import os
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
 
 r = requests.get("http://google.com")
 
 print(r.status_code)
-
-
-def greet(who_to_greet):
-    greetings = "Hello, {}".format(who_to_greet)
-    return greetings
+print(r.ok)
